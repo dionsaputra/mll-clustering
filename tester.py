5b5bb2c63b0220c7aeb693f1d0c5ab0f54e4ceef
Harry <hamjaya_harry@yahoo.co.id>
2019-11-15 21:17:52 +0700

    add test for agglomerative

--- tester.py
+++ tester.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from dbscan import Dbscan
+from agglomerative import Agglomerative
 from sklearn import datasets
 
 
@@ -39,9 +40,50 @@ class Tester():
             if dbscan.labels[i] == self.exact_labels[i]:
                 accurate_sum += 1
 
-        print("akurasi: ", accurate_sum/len(self.exact_labels))
+        print("akurasi dbscan: ", accurate_sum/len(self.exact_labels))
+
+    def test_agglomerative(self):
+        agglomerative_single_model = Agglomerative(self.features, 3)
+        agglomerative_complete_model = Agglomerative(self.features, 3, 'complete')
+        agglomerative_average_model = Agglomerative(self.features, 3, 'average')
+        agglomerative_average_group_model = Agglomerative(self.features, 3, 'average_group')
+
+        agglomerative_single_label = agglomerative_single_model.get_cluster()
+        agglomerative_complete_label = agglomerative_complete_model.get_cluster()
+        agglomerative_average_label = agglomerative_average_model.get_cluster()
+        agglomerative_average_group_label = agglomerative_average_group_model.get_cluster()
+
+        accurate_sum_1 = 0
+        for i in range(len(agglomerative_single_label)):
+            # print(agglomerative_single_label[i], " ", self.exact_labels[i])
+            if agglomerative_single_label[i] == self.exact_labels[i]:
+                accurate_sum_1 += 1
+
+        accurate_sum_2 = 0
+        for i in range(len(agglomerative_complete_label)):
+            # print(agglomerative_complete_label[i], " ", self.exact_labels[i])
+            if agglomerative_complete_label[i] == self.exact_labels[i]:
+                accurate_sum_2 += 1
+        
+        accurate_sum_3 = 0
+        for i in range(len(agglomerative_average_label)):
+            # print(agglomerative_average_label[i], " ", self.exact_labels[i])
+            if agglomerative_average_label[i] == self.exact_labels[i]:
+                accurate_sum_3 += 1
+
+        accurate_sum_4 = 0
+        for i in range(len(agglomerative_average_group_label)):
+            # print(agglomerative_average_group_label[i], " ", self.exact_labels[i])
+            if agglomerative_average_group_label[i] == self.exact_labels[i]:
+                accurate_sum_4 += 1
+
+        print("akurasi agglomerative dengan single linkage: ", accurate_sum_1/len(self.exact_labels))
+        print("akurasi agglomerative dengan complete linkage: ", accurate_sum_2/len(self.exact_labels))
+        print("akurasi agglomerative dengan average linkage: ", accurate_sum_3/len(self.exact_labels))
+        print("akurasi agglomerative dengan average complete linkage: ", accurate_sum_4/len(self.exact_labels))
 
 
 if __name__ == "__main__":
     tester = Tester()
     tester.test_dbscan()
+    tester.test_agglomerative()
